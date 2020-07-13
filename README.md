84dc1fa6dc248d07a5bf52fa7bf3e507673e2335
Lam Nguyen <lamcnguyen89@gmail.com>
2020-07-13 18:00:08 -0400

    Built the server using express

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Note-Taker--Node-Server
-Create an application that can be used to write, save and delete notes. This application will use an express backend and save and retrieve note data from a JSON file
+Create an application that can be used to write, save and delete notes. This application will use an express backend and save and retrieve note data from a JSON file. <br />
+These are the functional pieces of the program:
+
+* Be able to write and save notes
+* Be able to delete notes
+
--- server.js
+++ server.js
@@ -2,10 +2,24 @@
 // SETUP AND OBTAIN DEPENDENCIES
 //=============================================
 
+var express = require("express")
 //=============================================
 // CREATE AND CONFIGURE SERVER
 // Set up the basic properties of the server
 //=============================================
+
+// Create the "express" server:
+var app = express();
+
+// Sets the initial Port that the server will listen through for client-side requests.
+// process.env.PORT is a command that means that the server will listen to whatever number is in the environmental variable PORT. 
+var PORT = process.env.PORT || 3000
+
+// Sets up the Express app to handle data parsing using middleware.
+// json and urlencoded are both part of bodyParse in Express: https://github.com/expressjs/body-parser
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json()); 
+
 
 //=============================================
 // BUILD ROUTES
@@ -13,17 +27,18 @@
 //=============================================
 
 // HTML Routes:
-
-    // GET `/notes` - Should return the `notes.html` file.
-    // GET `*` - Should return the `index.html` file
+require("./routes/htmlRoutes")
 
 // API Routes:
+require("./routes/apiRoutes")
 
-    // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-    // POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
-    // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
     
+
 //=============================================
 // START LISTENER
-// The code below starts our software server
+// The code below starts our software server. Almost like initializing a function after creating the function.
 //=============================================
+
+app.listen(PORT, function() {
+    console.log(`Listening on Port: ${PORT}`)
+} )

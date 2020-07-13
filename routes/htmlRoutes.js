12e399362b772b06d26aa9e8857ca1b2957a9347
Lam Nguyen <lamcnguyen89@gmail.com>
2020-07-13 18:09:53 -0400

    Added the html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,25 @@
 //=============================================
-// HTML Routes:
+// DEPENDENCIES
+// include the path module to allow the JS file to move to the correct path to the specified file
+//=============================================
+var PATH = require("path");
+
+//=============================================
+// ROUTING
 //=============================================
 
+module.exports = function(app) {
 
-// GET `/notes` - Should return the `notes.html` file.
+    // GET `/notes` - Should return the `notes.html` file:
+    app.get("/notes", function() {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
 
-// GET `*` - Should return the `index.html` file
+
+    // GET `*` - Should return the `index.html` file:
+    app.get("*", function() {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+};
+

6393d4743d25c828b5cba5ffe7b53b9233aaf23a
Lam Nguyen <lamcnguyen89@gmail.com>
2020-07-14 02:05:14 -0400

    Added a / get

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,6 +15,11 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
  
+    // GET "/" is index file
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
     // GET `*` - Should return the `index.html` file. I've got to remember that this * means "all other" and it has to go last in all the get and post requests. Otherwise nothing else will work
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
